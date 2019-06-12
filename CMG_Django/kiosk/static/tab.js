20a55fb696ebf63e8801979dd33525c9f0af22d5
SDEDU03 <SDEDU03@DESKTOP-FKT6DVV>
2019-06-12 18:27:25 +0900

    add order list modal

--- CMG_Django/kiosk/static/tab.js
+++ CMG_Django/kiosk/static/tab.js
@@ -1,19 +1,41 @@
-function getList(tab, url){
+var order_list = [];
+
+var orders="";
+function getMultiList(tab, url){
 	$.getJSON(
 		url,
 		function(data){
 			var str = "";
 			$.each(data, function(key, val){
-				str+=key+" "+val+"<br>";
+				str+="<input name="+tab+"_sel type=checkbox value=\""+key+"\">"+key+val+"<br>";
 			})
 			$(tab).html(str);
 		}
 	);
 }
 
-$(document).ready(function(){
+function getList(tab, url){
+	$.getJSON(
+		url,
+		function(data){
+			var str = "";
+			$.each(data, function(key, val){
+				str+="<input type=radio name="+tab+"_sel value=\""+key+"\">"+key+val+"<br>";
+			})
+			$(tab).html(str);
+		}
+	);
+}
+
+function init(){
 	getList("#menu", "getmenu");
-});
+	getList("#bread", "getbread");
+	getMultiList("#vegetable", "getvege");
+	getMultiList("#source", "getsrc");
+}
+
+$(document).ready(init());
+
 $("#menu_tab").click(function(){
 	document.getElementById("menu_tab").className = "nav-link active";
 	document.getElementById("bread_tab").className = "nav-link";
@@ -24,7 +46,6 @@ $("#menu_tab").click(function(){
 	document.getElementById("bread").className = "tab-pane fade";
 	document.getElementById("vegetable").className = "tab-pane fade";
 	document.getElementById("source").className = "tab-pane fade";
-	getList("#menu", "getmenu");
 });
 
 $("#bread_tab").click(function(){
@@ -37,7 +58,6 @@ $("#bread_tab").click(function(){
 	document.getElementById("bread").className = "tab-pane fade show active";
 	document.getElementById("vegetable").className = "tab-pane fade";
 	document.getElementById("source").className = "tab-pane fade";
-	getList("#bread", "getbread");
 });
 
 $("#vegetable_tab").click(function(){
@@ -50,7 +70,6 @@ $("#vegetable_tab").click(function(){
 	document.getElementById("bread").className = "tab-pane fade";
 	document.getElementById("vegetable").className = "tab-pane fade show active";
 	document.getElementById("source").className = "tab-pane fade";
-	getList("#vegetable", "getvege");
 });
 
 $("#source_tab").click(function(){
@@ -63,5 +82,41 @@ $("#source_tab").click(function(){
 	document.getElementById("bread").className = "tab-pane fade";
 	document.getElementById("vegetable").className = "tab-pane fade";
 	document.getElementById("source").className = "tab-pane fade show active";
-	getList("#source", "getsrc");
 });
+
+function list(name){
+	var len = document.getElementsByName(name).length;
+	var result = [];
+	for(i = 0; i<len; i++){
+		if(document.getElementsByName(name)[i].checked == true)
+			result.push(document.getElementsByName(name)[i].value);
+	}
+	return result;
+}
+
+function add(){
+	order_list.push({
+		menu : list("#menu_sel"),
+		bread : list("#bread_sel"),
+		vegetable : list("#vegetable_sel"),
+		source : list("#source_sel")
+	});
+	orders+="<a href=# class=list-group-item>주문주문</a>"
+	$(order).html(orders);
+	init();
+}
+
+function sub(){
+	$('#ordermodal').show();
+	var str = "";
+	for(i = 0; i<order_list.length; i++){
+		str += order_list[i].menu + order_list[i].bread 
+		+ order_list[i].vegetable + order_list[i].source;
+	}
+	$(listAll).html(str);
+	init();
+}
+
+function hide(){
+	$("#ordermodal").hide();
+}
--- CMG_Django/kiosk/views.py
+++ CMG_Django/kiosk/views.py
@@ -8,25 +8,21 @@ def kiosk(request):
 
 def getMenu(request):
     get = {}
-    for e in Menu.objects.all():
-        get[e.name] = e.img
+    for e in Menu.objects.all(): get[e.name] = e.img
     return HttpResponse(json.dumps(get), content_type='application/json')
   
 def getBread(request):
     get = {}
-    for e in Bread.objects.all():
-        get[e.name] = e.img
+    for e in Bread.objects.all(): get[e.name] = e.img
     return HttpResponse(json.dumps(get), content_type='application/json')
   
 def getVege(request):
     get = {}
-    for e in Vegetable.objects.all():
-        get[e.name] = e.img
+    for e in Vegetable.objects.all(): get[e.name] = e.img
     return HttpResponse(json.dumps(get), content_type='application/json')
   
 def getSource(request):
     get = {}
-    for e in Source.objects.all():
-        get[e.name] = e.img
+    for e in Source.objects.all(): get[e.name] = e.img
     return HttpResponse(json.dumps(get), content_type='application/json')
     
