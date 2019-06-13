ab452276c8d78eaf9ac83d8c7f37a7c71f2fab56
SDEDU03 <SDEDU03@DESKTOP-FKT6DVV>
2019-06-13 10:47:36 +0900

    add part order list modal

--- CMG_Django/kiosk/static/tab.js
+++ CMG_Django/kiosk/static/tab.js
@@ -1,39 +1,3 @@
-var order_list = [];
-
-var orders="";
-function getMultiList(tab, url){
-	$.getJSON(
-		url,
-		function(data){
-			var str = "";
-			$.each(data, function(key, val){
-				str+="<input name="+tab+"_sel type=checkbox value=\""+key+"\">"+key+val+"<br>";
-			})
-			$(tab).html(str);
-		}
-	);
-}
-
-function getList(tab, url){
-	$.getJSON(
-		url,
-		function(data){
-			var str = "";
-			$.each(data, function(key, val){
-				str+="<input type=radio name="+tab+"_sel value=\""+key+"\">"+key+val+"<br>";
-			})
-			$(tab).html(str);
-		}
-	);
-}
-
-function init(){
-	getList("#menu", "getmenu");
-	getList("#bread", "getbread");
-	getMultiList("#vegetable", "getvege");
-	getMultiList("#source", "getsrc");
-}
-
 $(document).ready(init());
 
 $("#menu_tab").click(function(){
@@ -84,6 +48,46 @@ $("#source_tab").click(function(){
 	document.getElementById("source").className = "tab-pane fade show active";
 });
 
+var order_list = [];
+
+var orders="";
+function getMultiList(tab, url){
+	$.getJSON(
+		url,
+		function(data){
+			var str = "";
+			$.each(data, function(key, val){
+				if(tab == "#vegetable"){
+					str+="<input name="+tab+"_sel type=checkbox value=\""+key+"\"checked>"+key+val+"<br>";
+				}else{
+					str+="<input name="+tab+"_sel type=checkbox value=\""+key+"\">"+key+val+"<br>";
+				}
+			})
+			$(tab).html(str);
+		}
+	);
+}
+
+function getList(tab, url){
+	$.getJSON(
+		url,
+		function(data){
+			var str = "";
+			$.each(data, function(key, val){
+				str+="<input type=radio name="+tab+"_sel value=\""+key+"\">"+key+val+"<br>";
+			})
+			$(tab).html(str);
+		}
+	);
+}
+
+function init(){
+	getList("#menu", "getmenu");
+	getList("#bread", "getbread");
+	getMultiList("#vegetable", "getvege");
+	getMultiList("#source", "getsrc");
+}
+
 function list(name){
 	var len = document.getElementsByName(name).length;
 	var result = [];
@@ -95,28 +99,44 @@ function list(name){
 }
 
 function add(){
+	var num = order_list.length;
 	order_list.push({
 		menu : list("#menu_sel"),
 		bread : list("#bread_sel"),
 		vegetable : list("#vegetable_sel"),
 		source : list("#source_sel")
 	});
-	orders+="<a href=# class=list-group-item>주문주문</a>"
+	orders+="<a href=# onclick=showList("+num+") class=list-group-item>"+(num+1)+"번 주문서</a>"
 	$(order).html(orders);
 	init();
 }
 
-function sub(){
+function showList(num){
 	$('#ordermodal').show();
-	var str = "";
-	for(i = 0; i<order_list.length; i++){
-		str += order_list[i].menu + order_list[i].bread 
-		+ order_list[i].vegetable + order_list[i].source;
-	}
-	$(listAll).html(str);
-	init();
+	var str = "메뉴: "+order_list[num].menu+"<br>" 
+		+"빵: "+order_list[num].bread+"<br>" 
+		+"야채: "+order_list[num].vegetable+"<br>"
+		+"소스: "+order_list[num].source;
+	$(listPart).html(str);
 }
 
-function hide(){
-	$("#ordermodal").hide();
+function sub(){
+	var str = "";
+	for(i = 0; i<order_list.length; i++){
+		str += "메뉴: "+order_list[i].menu+"<br>" 
+		+"빵: "+order_list[i].bread+"<br>" 
+		+"야채: "+order_list[i].vegetable+"<br>"
+		+"소스: "+order_list[i].source+"<br>"
+		+"=================================================<br>";
+	}
+	$(listAll).html(str);
+	$('#submitmodal').show();
+}
+
+function doOrder(){
+	
+}
+
+function hide(mod){
+	$(mod).hide();
 }
